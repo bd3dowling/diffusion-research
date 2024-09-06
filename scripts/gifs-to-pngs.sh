6d0e32448f34d67b06bcfa625e86360b9fa3a570
Ben Dowling <ben.dowling@icloud.com>
2024-09-06 13:52:59 +0100

    feat(presentation): scaffold

--- scripts/gifs-to-pngs.sh
+++ scripts/gifs-to-pngs.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
 magick convert -coalesce \
     presentation/assets/gmm-posterior-smc_diff_opt/gmm-posterior-smc_diff_opt.gif \
     presentation/assets/gmm-posterior-smc_diff_opt/gmm-posterior-smc_diff_opt.png
+
+magick convert -coalesce \
+    presentation/assets/gmm-posterior-vjp_guidance/gmm-posterior-vjp_guidance.gif \
+    presentation/assets/gmm-posterior-vjp_guidance/gmm-posterior-vjp_guidance.png
+
+magick convert -coalesce \
+    presentation/assets/gmm-prior/gmm-prior.gif \
+    presentation/assets/gmm-prior/gmm-prior.png
